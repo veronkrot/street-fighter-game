c3e323a36c905b85f936951518c1abe97837d7ba
Veronika Krotenko <49248836+veronkrot@users.noreply.github.com>
2019-11-01 19:02:23 +0200

    added delete fighter feature (#10)

--- server/routes/details.js
+++ server/routes/details.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
-const {getFighterById} = require('../services/fighters.services');
+const {getFighterById, deleteFighterById} = require('../services/fighters.services');
 
-router.get('/fighter/:id.json', function (req, res, next) {
+router.get('/:id', function (req, res, next) {
     const id = req.params.id;
     const fighter = getFighterById(id);
     if (fighter) {
         res.send(fighter);
+    } else {
+        res.status(400).send('Some error');
+    }
+});
+
+router.delete('/:id', function (req, res, next) {
+    const id = req.params.id;
+    if (!id) {
+        res.status(400).send('Some error');
+    }
+    const result = deleteFighterById(id);
+    if (result) {
+        res.status(200).send("Done");
     } else {
         res.status(400).send('Some error');
     }
--- server/services/fighters.services.js
+++ server/services/fighters.services.js
@@ -1,14 +1,42 @@
 const fs = require('fs');
 
+const fightersJsonPath = 'resources/api/fighters.json';
+const detailsPath = `../resources/api/details/fighter/`;
+
+const createDetailsPath = (id) => {
+    return `${detailsPath}${id}.json`;
+};
+
 const getFighters = () => {
     return require('../resources/api/fighters');
 };
 
 const getFighterById = (id) => {
-    return require(`../resources/api/details/fighter/${id}.json`);
+    return require(createDetailsPath(id));
+};
+
+const deleteFighterById = (id) => {
+    try {
+        const fighters = fs.readFileSync(fightersJsonPath);
+        let fightersArray = JSON.parse(fighters);
+        for (let i = 0; i < fightersArray.length; i++) {
+            if (fightersArray[i]._id === id) {
+                fightersArray.splice(i, 1);
+                break;
+            }
+        }
+        const jsonFighters = JSON.stringify(fightersArray);
+        fs.writeFileSync(fightersJsonPath, jsonFighters);
+        fs.unlinkSync(`resources/api/details/fighter/${id}.json`);
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
 };
 
 module.exports = {
     getFighters,
-    getFighterById
+    getFighterById,
+    deleteFighterById
 };
